ace834ce923a80905dead70891a39c1cae067787
ali <ali@ubuntu-2204.linuxvmimages.local>
2026-04-09 17:29:30 +0500

    Initial commit - Phase 1 & Phase 2 setup (Cook Desk)

---
+++ cook_desk/cook_desk/doctype/kitchen_printer/kitchen_printer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Pixymo Tech and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class KitchenPrinter(Document):
+	pass
